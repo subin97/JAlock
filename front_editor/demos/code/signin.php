5bfa701d8f7fee895e5b7abaec3b9e0c7ca1a58d
Subin Choi <subin_97@likelion.org>
2018-11-23 17:21:11 +0900

    signin and signup

--- front_editor/demos/code/signin.php
+++ front_editor/demos/code/signin.php
@@ -120,17 +120,20 @@ color: #777;
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card card-signin my-5">
           <div class="card-body">
-            <h5 class="card-title text-center">Sign In</h5>
-            <form class="form-signin">
+            <h5 class="card-title text-center">로그인</h5>
+            <form class="form-signin" method="post" action="/jalock/front_editor/demos/code/signin_back.php">
               <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputEmail">Email address</label>
+                <input type="text" id="inputEmail" name="nickname" class="form-control" placeholder="Your nickname" required autofocus>
+                <label for="inputEmail">닉네임</label>
               </div>
               <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <label for="inputPassword">Password</label>
+                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+                <label for="inputPassword">비밀번호</label>
               </div>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">로그인</button>
+            </form>
+            <form class="form-signin" >
+              <a href="/jalock/front_editor/demos/code/signup.php" class="btn btn-lg btn-primary btn-block text-uppercase">회원가입</a>
             </form>
           </div>
         </div>
--- front_editor/demos/code/signup_back.php
+++ front_editor/demos/code/signup_back.php
@@ -3,16 +3,14 @@
   if (mysqli_connect_errno()){
       echo "연결실패<br>".mysqli_connect_error();
   }
-	//include "../db.php";
-	//include "../password.php";
 
-	$email = $_POST['email'];
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	//  $adress = $_POST['adress'];
-  //	$email = $_POST['email'].'@'.$_POST['emadress'];
+	$nickname = $_POST["nickname"];
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-  $sql = mq("insert into user (email, password) values('".$email."','".$password."')");
+  $query ="insert into user (nickname, password, current) values ('$nickname', '$password', 1)";
+  $result = mysqli_query($conn, $query);
+
 ?>
 <meta charset="utf-8" />
 <script type="text/javascript">alert('회원가입이 완료되었습니다.');</script>
-<meta http-equiv="refresh" content="0 url=/">
+<meta http-equiv="refresh" content="0 url=/jalock/front_editor/demos/code/index_front.php?id=1&code=">
